ee1a0a3ce784a2f0393b5451cb1fc3dd34a76266
Data Juggler <corby@datajuggler.com>
2019-09-29 14:03:12 -0500

    I refactored the code a little so the Main Form is easier to swtich between projects.

--- SQL/SetFieldValues.sql
+++ SQL/SetFieldValues.sql
@@ -1,13 +1,16 @@
 Declare @TableId int
-Set @TableId = 1 -- change to your TableId
+Set @TableId = 3 -- change to your TableId    Example: Select * From DTNTable to find your table
 
 Declare @PrimaryKeyFieldOrdinal int
 Set @PrimaryKeyFieldOrdinal = (Select FieldOrdinal From DTNField Where PrimaryKey = 1 And TableId = @TableId) 
 
 -- This query build the PropertyName = Value pairs for C# insert statements
 Select '                    rawImport.' + FieldName + ' = words[' + Cast((FieldOrdinal - 1) As nvarchar) + '].Text;' from DTNField
-Where TableId = 1 And PrimaryKey = 0 And FieldOrdinal < @PrimaryKeyFieldOrdinal
+Where TableId = @TableId And PrimaryKey = 0 And FieldOrdinal < @PrimaryKeyFieldOrdinal
 Union All
 Select '                    rawImport.' + FieldName + ' = words[' + Cast((FieldOrdinal - 2) As nvarchar) + '].Text;' from DTNField
-Where TableId = 1 And PrimaryKey = 0 And FieldOrdinal > @PrimaryKeyFieldOrdinal
+Where TableId = @TableId And PrimaryKey = 0 And FieldOrdinal > @PrimaryKeyFieldOrdinal
+
+
+
 
